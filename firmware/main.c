7e8fb610f4c6695965d6b504acaf0b22ca4a37b1
Roman Moravčík <roman.moravcik@gmail.com>
2016-10-27 21:56:21 +0200

    firmware: first cleanup of code.

--- firmware/main.c
+++ firmware/main.c
@@ -17,86 +17,99 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/eeprom.h>
 #include <util/delay.h>
 
 #include "ds_rtc_lib/library-gcc/twi.h"
 #include "ds_rtc_lib/library-gcc/rtc.h"
-
 #include "ds_rtc_lib/library-gcc/test/uart.h"
 
-#define LED_BIT PB5
-#define LED_DDR DDRB
-#define LED_PORT PORTB
-#define LED_HIGH LED_PORT |= _BV(LED_BIT)
-#define LED_HIGH LED_PORT |= _BV(LED_BIT)
-#define LED_LOW  LED_PORT &= ~(_BV(LED_BIT))
+#include "common.h"
 
-//void read_rtc(void)
-//{
-//	uint8_t hour, min, sec;
-//
-//	rtc_get_time_s(&hour, &min, &sec);
-//}
+#define DEBUG 1
 
-void main(void) __attribute__ ((noreturn));
+typedef struct {
+    uint8_t hour;
+    uint8_t min;
+} alarm_time_t;
 
-void main(void)
+typedef struct {
+    alarm_time_t alarm[7];
+} conf_t;
+
+static conf_t EEMEM eeprom_conf;
+static conf_t conf;
+
+void uart_rx_handler(unsigned char c)
 {
-	struct tm* t = NULL;
-	struct tm time;
-	char buf[32];
-	uint8_t hour, min, sec;
+}
 
-	uartInit();
-	uartSetBaudRate(115200);
-	uartSendString("DS RTC Library Test\r\n");
-
-	LED_DDR  |= _BV(LED_BIT); // indicator led
-
-	for (int i = 0; i < 5; i++) {
-		LED_HIGH;
-		_delay_ms(100);
-		LED_LOW;
-		_delay_ms(100);
-	}
-
-	twi_init_master();
-	rtc_init();
-
-#if 0
-	rtc_set_time_s(12, 0, 50);
-
-	time.hour = 11;
-	time.min = 24;
-	time.sec = 0;
-
-	time.mday = 23;
-	time.mon = 10;
-	time.year = 1917;
-
-	rtc_set_time(&time);
+int main(void)
+{
+    struct tm* time = NULL;
+    uint8_t wday = 0;
+#ifdef DEBUG
+    char buf[32];
 #endif
 
-	rtc_set_alarm_s(12, 1, 0);
-	
-	rtc_get_alarm_s(&hour, &min, &sec);
+    uartInit();
+    uartSetBaudRate(115200);
+    uartSetRxHandler(&uart_rx_handler);
 
-	sprintf(buf, "Alarm is set -%02d:%02d:%02d-\r\n", hour, min, sec);
-	uartSendString(buf);
-	uartSendString("---\r\n");
-	uartSendString("---\r\n");
-	uartSendString("---\r\n");
+#ifdef DEBUG
+    uartSendString("DS RTC Library Test\r\n");
+#endif
 
-	while (1) {
-		t = rtc_get_time();
+    twi_init_master();
+    rtc_init();
 
-		sprintf(buf, "%02d:%02d:%02d\r\n", t->hour, t->min, t->sec);
-		uartSendString(buf);
-		uartSendString("---\r\n");
+    eeprom_read_block(&conf, &eeprom_conf, sizeof(conf_t));
 
-		if (rtc_check_alarm())
-			uartSendString("ALARM!\r\n");
+#if 0
+    rtc_set_time_s(12, 0, 50);
 
-	   	_delay_ms(500);
-	}
+    time.hour = 11;
+    time.min = 24;
+    time.sec = 0;
+
+    time.mday = 23;
+    time.mon = 10;
+    time.year = 1917;
+
+    rtc_set_time(&time);
+#endif
+
+    while (1) {
+        time = rtc_get_time();
+
+#ifdef DEBUG
+        sprintf(buf, "Time: %02d:%02d:%02d\r\n", time->hour, time->min, time->sec);
+        uartSendString(buf);
+#endif
+
+        if (time->wday != wday) {
+            rtc_set_alarm_s(conf.alarm[time->wday - 1].hour,
+                            conf.alarm[time->wday - 1].min,
+                            0);
+
+#ifdef DEBUG
+            sprintf(buf, "Alarm is set: %02d:%02d:%02d\r\n",
+                    conf.alarm[time->wday - 1].hour,
+                    conf.alarm[time->wday - 1].min,
+                    0);
+            uartSendString(buf);
+#endif
+
+            wday = time->wday;
+        }
+
+        if (rtc_check_alarm()) {
+#ifdef DEBUG
+            uartSendString("ALARM!\r\n");
+#endif
+            // FIXME
+        }
+
+        _delay_ms(500);
+    }
 }
