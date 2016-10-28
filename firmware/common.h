4f1ade2e6f7a2aa5585d2116b5926ae191429694
Roman Moravčík <roman.moravcik@gmail.com>
2016-10-28 21:00:00 +0200

    firmware: added first commands

--- firmware/common.h
+++ firmware/common.h
@@ -1,4 +1,35 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <avr/eeprom.h>
+
+//#define DEBUG 1
+
+#define ALARM_OFF_HOUR 99
+#define ALARM_OFF_MIN 99
+
+#define COMMAND_GET_STATUS "STAT?"
+#define COMMAND_GET_STATUS_RSP "STAT+"
+
+#define COMMAND_SET_ALARM "ALARM+"
+#define COMMAND_SET_ALARM_RSP "ALARM+OK"
+
+#define COMMAND_SET_DATE "DATE+"
+#define COMMAND_SET_DATE_RSP "DATE+OK"
+
+typedef struct {
+    uint8_t hour;
+    uint8_t min;
+} alarm_time_t;
+
+typedef struct {
+    alarm_time_t alarm[7];
+} conf_t;
+
+#define SET_ALARM_ID_MASK 0x07
+#define SET_ALARM 0x10
+#define SET_DATE 0x20
+
+#define WDAY_TO_ID(x) (x - 1)
+
 #endif
--- firmware/main.c
+++ firmware/main.c
@@ -24,24 +24,60 @@
 #include "ds_rtc_lib/library-gcc/rtc.h"
 #include "ds_rtc_lib/library-gcc/test/uart.h"
 
+#include "ssd1306xled/ssd1306xled/ssd1306xled.h"
+
+#include "command.h"
 #include "common.h"
 
-#define DEBUG 1
-
-typedef struct {
-    uint8_t hour;
-    uint8_t min;
-} alarm_time_t;
-
-typedef struct {
-    alarm_time_t alarm[7];
-} conf_t;
-
 static conf_t EEMEM eeprom_conf;
-static conf_t conf;
+conf_t conf;
 
-void uart_rx_handler(unsigned char c)
+uint16_t status = 0;
+
+struct tm set_time;
+
+ISR(INT1_vect, ISR_NOBLOCK)
 {
+    uartSendString("Snooze!\r\n");
+}
+
+ISR(PCINT1_vect, ISR_NOBLOCK)
+{
+    uartSendString("Tick!\r\n");
+}
+
+void rtc_SQW_irq_init(void)
+{
+    DDRC &= ~_BV(PC2);
+
+    PCMSK1 |= _BV(PCINT10);
+    PCICR |= _BV(PCIE1);
+}
+
+void snooze_irq_init(void)
+{
+    DDRD &= ~_BV(PD3);
+    PORTD |= _BV(PD3);
+
+    EICRA |= _BV(ISC11);
+    EIMSK |= _BV(INT1);
+}
+
+void eeprom_init(void)
+{
+    uint8_t id = 0;
+
+    eeprom_read_block(&conf, &eeprom_conf, sizeof(conf_t));
+
+    for (id = 0; id < 7; id++) {
+        if ((conf.alarm[id].hour == 0xff) &&
+            (conf.alarm[id].min == 0xff)) {
+            conf.alarm[id].hour = ALARM_OFF_HOUR;
+            conf.alarm[id].min = ALARM_OFF_MIN;
+        }
+    }
+
+    eeprom_write_block(&conf, &eeprom_conf, sizeof(conf_t));
 }
 
 int main(void)
@@ -49,38 +85,69 @@ int main(void)
     struct tm* time = NULL;
     uint8_t wday = 0;
 #ifdef DEBUG
-    char buf[32];
+    char buf[50];
 #endif
 
     uartInit();
     uartSetBaudRate(115200);
-    uartSetRxHandler(&uart_rx_handler);
-
-#ifdef DEBUG
-    uartSendString("DS RTC Library Test\r\n");
-#endif
+    uartSetRxHandler(&command_rx_handler);
 
     twi_init_master();
+
     rtc_init();
 
-    eeprom_read_block(&conf, &eeprom_conf, sizeof(conf_t));
+    rtc_SQW_irq_init();
+    rtc_SQW_set_freq(FREQ_1);
+    rtc_SQW_enable(true);
 
-#if 0
-    rtc_set_time_s(12, 0, 50);
+    snooze_irq_init();
 
-    time.hour = 11;
-    time.min = 24;
-    time.sec = 0;
+    eeprom_init();
 
-    time.mday = 23;
-    time.mon = 10;
-    time.year = 1917;
-
-    rtc_set_time(&time);
-#endif
+    sei();
 
     while (1) {
         time = rtc_get_time();
+
+        if (status & SET_ALARM) {
+            uint8_t id = status & SET_ALARM_ID_MASK;
+
+#ifdef DEBUG
+            sprintf(buf, "SET ALARM%d: Time %02u:%02u\r\n",
+                    id, set_time.hour, set_time.min);
+            uartSendString(buf);
+#endif
+
+            conf.alarm[id].hour = set_time.hour;
+            conf.alarm[id].min = set_time.min;
+
+            eeprom_write_block(&conf, &eeprom_conf, sizeof(conf_t));
+
+            if (id == WDAY_TO_ID(wday)) {
+                if ((conf.alarm[id].hour != ALARM_OFF_HOUR) &&
+                    (conf.alarm[id].min != ALARM_OFF_MIN)) {
+                    rtc_set_alarm_s(conf.alarm[id].hour,
+                                    conf.alarm[id].min,
+                                    0);
+                } else {
+                    rtc_reset_alarm();
+                }
+            }
+
+            status &= ~(SET_ALARM | SET_ALARM_ID_MASK);
+        }
+
+        if (status & SET_DATE) {
+#ifdef DEBUG
+            sprintf(buf, "SET DATE: Time %02u:%02u:%02u Date %02u:%02u:%04u (%1u)\r\n",
+                    set_time.hour, set_time.min, set_time.sec, set_time.mday,
+                    set_time.mon, set_time.year, set_time.wday);
+            uartSendString(buf);
+#endif
+
+            rtc_set_time(&set_time);
+            status &= ~SET_DATE;
+        }
 
 #ifdef DEBUG
         sprintf(buf, "Time: %02d:%02d:%02d\r\n", time->hour, time->min, time->sec);
@@ -88,17 +155,20 @@ int main(void)
 #endif
 
         if (time->wday != wday) {
-            rtc_set_alarm_s(conf.alarm[time->wday - 1].hour,
-                            conf.alarm[time->wday - 1].min,
-                            0);
+            if ((conf.alarm[WDAY_TO_ID(time->wday)].hour != ALARM_OFF_HOUR) &&
+                (conf.alarm[WDAY_TO_ID(time->wday)].min != ALARM_OFF_MIN)) {
+                rtc_set_alarm_s(conf.alarm[WDAY_TO_ID(time->wday)].hour,
+                                conf.alarm[WDAY_TO_ID(time->wday)].min,
+                                0);
 
 #ifdef DEBUG
-            sprintf(buf, "Alarm is set: %02d:%02d:%02d\r\n",
-                    conf.alarm[time->wday - 1].hour,
-                    conf.alarm[time->wday - 1].min,
-                    0);
-            uartSendString(buf);
+                sprintf(buf, "SET ALARM: %02d:%02d:%02d\r\n",
+                        conf.alarm[time->wday - 1].hour,
+                        conf.alarm[time->wday - 1].min,
+                        0);
+                uartSendString(buf);
 #endif
+            }
 
             wday = time->wday;
         }
